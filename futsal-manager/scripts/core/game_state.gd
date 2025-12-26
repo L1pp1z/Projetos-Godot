bc8d317292409ace1e6a54929e12f62a32acf6d3
L1pp1z <150986339+L1pp1z@users.noreply.github.com>
2025-12-26 10:11:15 -0300

    Cena Create Manager
    Cena incompleta:
    - Ja detalhada
    - Base para ideias seguintes
    
    Menu Principal:
    - Design Completo
    - Botoes NewGame Básico e Sair Funcional
    - Definição de Cores OK

---
+++ futsal-manager/scripts/core/game_state.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var coach := {
+	"first_name": "",
+	"last_name": "",
+	"age": 0,
+	"gender": "",
+	"continent": "",
+	"country": ""
+}
---
+++ futsal-manager/scripts/ui/coach_creation.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+
+
+#func _on_continue_pressed():
+	
